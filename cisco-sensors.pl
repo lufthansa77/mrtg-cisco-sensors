35278a7fca95d037343b903c64672a6e5e52f2ee
Your Name <you@example.com>
2017-05-15 11:09:02 +0200

    Funkcni nasazena verze

--- cisco-sensors.pl
+++ cisco-sensors.pl
@@ -1,6 +1,7 @@
-# Cisco sensors for mrtg module  v0.001
-
-# cfgmaker --host-template=cisco.htp
+# Cisco sensors for mrtg module  v0.001 - 11.5.2017
+# DC
+#
+# cfgmaker --host-template=cisco-sensors.htp
 
 use Data::Dump qw(pp);
 
@@ -55,7 +56,7 @@ my @precision = ( snmpwalk( $router, $v3opt, c_oid_entSensorPrecision ) );
 my @scale     = ( snmpwalk( $router, $v3opt, c_oid_entSensorScale ) );
 my @name      = ( snmpwalk( $router, $v3opt, c_oid_entPhysicalName ) );
 
-sub save_to_hash {                                                          # {{{
+sub save_to_hash {  # {{{
     my ( $arry, $key ) = @_;
 
     foreach my $line (@$arry) {
@@ -100,16 +101,6 @@ foreach my $index ( keys %{$sensors} ) {
 #pp(@value);
 #pp($sensors);
 
-#  1131 => {
-#            name      => "subslot 0/0 transceiver 3 Temperature Sensor",
-#            precision => 3,
-#            scale     => 9,
-#            scale_str => "units",
-#            status    => 1,
-#            type      => 8,
-#            type_str  => "celsius",
-#            value     => 29656,
-#          },
 #  1132 => {
 #            name => "subslot 0/0 transceiver 3 Supply Voltage Sensor",
 #            oid => "1.3.6.1.4.1.9.9.91.1.1.1.1.4.1132",
@@ -133,7 +124,7 @@ foreach my $index ( sort { $a <=> $b } keys %{$sensors} ) {
     my $name      = $rec->{name};
     my $oid       = $rec->{oid};
 
-    my $file_name = $router_name . "_$index";    #vygenerovat nahodne hash ?
+    my $file_name = $router_name. "_sensor_" . $index;
 
     $target_lines .= "# Sensor  $name\n";
     if ( $value > 0 ) {
@@ -175,7 +166,136 @@ PageTop[$file_name]: <h2>$sysname</h2>
           <td>$name ($type_str)</td>
      </tr></table></div>
 ECHO
-      ;
-
-    #last;
+;
 }
+##### Pavouk CPU, MEM
+$head_lines .= <<ECHO
+ECHO
+;
+my (@entid) = snmpwalk($router,$v3opt,'1.3.6.1.4.1.9.9.109.1.1.1.1.2');
+my %entids;
+foreach my $ent (@entid) {
+   $ent =~ /(\d+):(\d+)/;
+   my $instance= $1;
+   my $entindex = $2;
+        if ($entindex eq "0") {
+                $entids{$instance}="CPU";
+                next;
+        }
+   my ($entname) = snmpget($router,$v3opt,'1.3.6.1.2.1.47.1.1.1.1.7.'.$entindex);
+   $entids{$instance}=$entname;
+}
+
+my %cpu;
+my (@cputemp) = snmpwalk($router,$v3opt,'1.3.6.1.4.1.9.9.109.1.1.1.1.7');
+foreach my $cputempi(@cputemp) {
+   if ($cputempi eq "") { next }
+   $cputempi =~ /(\d+):\d+/;
+   my $instance=$1;
+   my $target_name=$router_name.".cpu".$instance;
+   $cpu{$instance}++;
+   $target_lines .= <<ECHO
+
+# $sysname Processor Load - $entids{$instance}
+Target[$target_name]: 1.3.6.1.4.1.9.9.109.1.1.1.1.7.$instance&1.3.6.1.4.1.9.9.109.1.1.1.1.8.$instance:$router
+MaxBytes[$target_name]: 100
+Options[$target_name]:  gauge,nobanner
+WithPeak[$target_name]: wmy
+YLegend[$target_name]: % Utilization
+ShortLegend[$target_name]: %
+Legend1[$target_name]: Avg 1 Minute Load
+Legend2[$target_name]: Avg 5 Minute Load
+Legend3[$target_name]: Max 1 Minute Load
+Legend4[$target_name]: Max 5 Minute Load
+LegendI[$target_name]:  1min :
+LegendO[$target_name]:  5min :
+Title[$target_name]: $sysname $entids{$instance} CPU load
+PageTop[$target_name]: <h1>$entids{$instance} Processor Load on $sysname</h1>
+AddHead[$target_name]:<link rel="stylesheet" type="text/css" href="../css/14all.css" />
+PageFoot[$target_name]:<div>MRTG &copy;<a href="http://oss.oetiker.ch/mrtg/">Tobias Oetiker</a></div>
+ECHO
+;
+}
+# Memory
+my (@memtemp) = snmpwalk($router,$v3opt,'1.3.6.1.4.1.9.9.48.1.1.1.2');
+foreach my $memtempi(@memtemp) {
+   if ($memtempi eq "") { next }
+   $memtempi =~ /(\d+):(.+)/;
+   my $instance=$1;
+   my $pool_name=$2;
+   my ($used, $free) = snmpget($router,$v3opt,'1.3.6.1.4.1.9.9.48.1.1.1.5.'.$instance, '1.3.6.1.4.1.9.9.48.1.1.1.6.'.$instance);
+   my $target_name=$router_name.".memory".$instance;
+   my $maxsize = $used+$free;
+   $target_lines .= <<MEM
+
+# $sysname $pool_name memory utilization
+Target[$target_name]: 1.3.6.1.4.1.9.9.48.1.1.1.6.$instance&1.3.6.1.4.1.9.9.48.1.1.1.5.$instance:$router
+YLegend[$target_name]: Bytes
+Options[$target_name]: gauge,nobanner
+MaxBytes[$target_name]: $maxsize
+ShortLegend[$target_name]: bytes
+Legend1[$target_name]: Avg $pool_name Free Mem
+Legend2[$target_name]: Avg $pool_name Used Mem
+Legend3[$target_name]: Max $pool_name Free Mem
+Legend4[$target_name]: Max $pool_name Used Mem
+LegendI[$target_name]: Free
+LegendO[$target_name]: Used
+WithPeak[$target_name]: ymw
+Title[$target_name]: $sysname $pool_name memory
+PageTop[$target_name]: <h1>$pool_name memory on $sysname</h1>
+  <div id="sysdetails">
+    <table>
+      <tr><td>Memory size:</td><td>$maxsize Bytes</td></tr>
+    </table>
+  </div>
+AddHead[$target_name]:<link rel="stylesheet" type="text/css" href="../css/14all.css" />
+PageFoot[$target_name]:<div>MRTG &copy;<a href="http://oss.oetiker.ch/mrtg/">Tobias Oetiker</a></div>
+MEM
+;
+}
+# Temperature
+my (@temp) = snmpwalk($router,$v3opt,'1.3.6.1.4.1.9.9.13.1.3.1.2');
+foreach my $tempi(@temp) {
+   if ($tempi eq "") { next } 
+   $tempi =~ /(\d+):(.*)/;
+   my $instance=$1;
+   my $sensor_name=$2;
+   if ($sensor_name eq "") {
+        $sensor_name = "NO NAME";
+   }
+   my ($temperature) = snmpget($router,$v3opt,"1.3.6.1.4.1.9.9.13.1.3.1.3.$instance");
+   if ($temperature eq "") { next }
+   my ($threshold) = snmpget($router,$v3opt,"1.3.6.1.4.1.9.9.13.1.3.1.4.$instance");
+   if ($threshold eq ("" || "0")) {
+        $threshold = 100;
+   }
+   my $maxsize = ($threshold);
+   my $target_name = $router_name.".temp".$instance;
+   $target_lines .= <<TEMP
+
+# $sysname $sensor_name temperature
+Target[$target_name]: 1.3.6.1.4.1.9.9.13.1.3.1.3.$instance&1.3.6.1.4.1.9.9.13.1.3.1.4.$instance:$router
+YLegend[$target_name]: ℃
+Options[$target_name]: gauge,nobanner,nopercent
+MaxBytes[$target_name]: $maxsize
+ShortLegend[$target_name]: ℃
+Legend1[$target_name]: Avg $sensor_name Temp
+Legend2[$target_name]: Avg $sensor_name Threshold
+Legend3[$target_name]: Max $sensor_name Temp
+Legend4[$target_name]: Max $sensor_name Threshold
+LegendI[$target_name]: temp
+LegendO[$target_name]: temp
+WithPeak[$target_name]: ymw
+Title[$target_name]: $sysname $sensor_name temperature
+PageTop[$target_name]: <h1>$sensor_name temperature on $sysname</h1>
+  <div id="sysdetails">
+    <table>
+      <tr><td>Threshold Temperature:</td><td>$maxsize&#x2103;</td></tr>
+    </table>
+  </div>
+AddHead[$target_name]:<link rel="stylesheet" type="text/css" href="../css/14all.css" />
+PageFoot[$target_name]:<div>MRTG &copy;<a href="http://oss.oetiker.ch/mrtg/">Tobias Oetiker</a></div>
+TEMP
+;
+}
+
